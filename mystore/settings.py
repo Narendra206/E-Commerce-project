b155a35c142420fdd6d3d53fe15d39b13c751480
Narendra206 <narendrakavuri666@gmail.com>
2026-01-09 17:09:05 +0530

    Fix settings and make app production ready

--- mystore/settings.py
+++ mystore/settings.py
@@ -20,13 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('django-insecure-b2-w8q-38&@3c!b(%%8li64t6(q5su+z^ohv(q7@a9p^h6fw9d')
+SECRET_KEY = SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "django-insecure-dev-key-only-for-local"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
 
 
 # Application definition
